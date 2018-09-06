542e0c87657f25f2592d3084238cbbaaf5c002e0
FredericF <frederic.fauquembert@sfr.fr>
2018-09-06 15:23:17 +0200

    PHP 11

--- calc.php
+++ calc.php
@@ -3,56 +3,56 @@
 if (isset($_POST['addition'])) {
     if (!is_numeric($_POST['chiffre1']) == false && !is_numeric($_POST['chiffre2']) == false) {
         if (isset($_POST['chiffre1']) && (isset($_POST['chiffre2']))) {
-            $result1 = $_POST['chiffre1'];
-            $result2 = $_POST['chiffre2'];
-            $resulttotal = $result1 + $result2;
-            echo $result1 . " + " . $result2 . " = " . $resulttotal;
+            $number1 = $_POST['chiffre1'];
+            $number2 = $_POST['chiffre2'];
+            $result = $number1 + $number2;
+            echo $number1 . " + " . $number2 . " = " . $result;
         }
     } elseif (is_numeric($_POST['chiffre1']) == false || is_numeric($_POST['chiffre2']) == false) {
-        echo "ce n'est pas un chiffre";
+        echo "ce n'est pas un nombre";
     }
 }
 if (isset($_POST['soustraction'])) {
     if (!is_numeric($_POST['chiffre1']) == false && !is_numeric($_POST['chiffre2']) == false) {
         if (isset($_POST['chiffre1']) && (isset($_POST['chiffre2']))) {
-            $result1 = $_POST['chiffre1'];
-            $result2 = $_POST['chiffre2'];
-            $resulttotal = $result1 - $result2;
-            echo $result1 . " - " . $result2 . " = " . $resulttotal;
+            $number1 = $_POST['chiffre1'];
+            $number2 = $_POST['chiffre2'];
+            $result = $number1 - $number2;
+            echo $number1 . " - " . $number2 . " = " . $result;
         }
     } elseif (is_numeric($_POST['chiffre1']) == false || is_numeric($_POST['chiffre2']) == false) {
-        echo "ce n'est pas un chiffre";
+        echo "ce n'est pas un nombre";
     }
 }
 if (isset($_POST['multiplication'])) {
     if (!is_numeric($_POST['chiffre1']) == false && !is_numeric($_POST['chiffre2']) == false) {
         if (isset($_POST['chiffre1']) && (isset($_POST['chiffre2']))) {
-            $result1 = $_POST['chiffre1'];
-            $result2 = $_POST['chiffre2'];
-            $resulttotal = $result1 * $result2;
-            echo $result1 . " * " . $result2 . " = " . $resulttotal;
+            $number1 = $_POST['chiffre1'];
+            $number2 = $_POST['chiffre2'];
+            $result = $number1 * $number2;
+            echo $number1 . " * " . $number2 . " = " . $result;
         }
-    } elseif (!is_numeric($_POST['chiffre1']) == true || !is_numeric($_POST['chiffre2']) == true) {
-        echo "ce n'est pas un chiffre";
+    } elseif (is_numeric($_POST['chiffre1']) == false || is_numeric($_POST['chiffre2']) == false) {
+        echo "ce n'est pas un nombre";
     }
 }
 if (isset($_POST['division'])) {
     if (!is_numeric($_POST['chiffre1']) == false && !is_numeric($_POST['chiffre2']) == false) {
         if (isset($_POST['chiffre1']) && (isset($_POST['chiffre2']))) {
-            $result1 = $_POST['chiffre1'];
-            $result2 = $_POST['chiffre2'];
-            if ($result2 != 0) {
-                $resulttotal = $result1 / $result2;
-                echo $result1 . " / " . $result2 . " = " . $resulttotal;
+            $number1 = $_POST['chiffre1'];
+            $number2 = $_POST['chiffre2'];
+            if ($number2 != 0) {
+                $result = $number1 / $number2;
+                echo $number1 . " / " . $number2 . " = " . $result;
             } else {
                 echo "Impossible! division par 0";
             }
         }
-    } elseif (is_numeric($_POST['chiffre1']) || is_numeric($_POST['chiffre2'])) {
-        echo "ce n'est pas un chiffre";
+    } elseif (is_numeric($_POST['chiffre1']) == false || is_numeric($_POST['chiffre2']) == false) {
+        echo "ce n'est pas un nombre";
     }
 }
 if (isset($_POST['raz'])) {
-    $resulttotal = 0;
+    $result = 0;
 }
 ?>
